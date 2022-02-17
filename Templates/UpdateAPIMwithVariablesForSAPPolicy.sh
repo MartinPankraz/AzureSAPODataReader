442135a1cea1c17b0c85c37a7257f64f2b48948e
MartinPankraz <mapankra@microsoft.com>
2022-02-17 14:21:42 +0100

    new lines deleted to avoid encoding errors

--- Templates/UpdateAPIMwithVariablesForSAPPolicy.sh
+++ Templates/UpdateAPIMwithVariablesForSAPPolicy.sh
@@ -10,21 +10,12 @@ export SAPOAuthClientSecret="<your SAP Oauth client secret (check TC OAUTH2)>"
 export SAPOAuthScope="<your SAP OAuth Scope (check TC OAUTH2)>"
 export SAPOAuthServerAdressForTokenEndpoint="<ip or domain>:<SAP backend ssl port>"
 export SAPOAuthRefreshExpiry="<refresh token expiry in seconds (take from SOAUTH2 transaction)>"
-
 az apim nv create --display-name 'AADTenantId' --named-value-id AADTenantId -g $resourceGroup --service-name $APIMserviceName --secret false --subscription $AzureSubscription --value $AADTenantId
-
 az apim nv create --display-name 'APIMAADRegisteredAppClientId' --named-value-id APIMAADRegisteredAppClientId -g $resourceGroup --service-name $APIMserviceName --secret false --subscription $AzureSubscription --value $APIMAADRegisteredAppClientId
-
 az apim nv create --display-name 'APIMAADRegisteredAppClientSecret' --named-value-id APIMAADRegisteredAppClientSecret -g $resourceGroup --service-name $APIMserviceName --secret true --subscription $AzureSubscription --value $APIMAADRegisteredAppClientSecret
-
 az apim nv create --display-name 'AADSAPResource' --named-value-id AADSAPResource -g $resourceGroup --service-name $APIMserviceName --secret false --subscription $AzureSubscription --value $AADSAPResource
-
 az apim nv create --display-name 'SAPOAuthClientID' --named-value-id SAPOAuthClientID -g $resourceGroup --service-name $APIMserviceName --secret false --subscription $AzureSubscription --value $SAPOAuthClientID
-
 az apim nv create --display-name 'SAPOAuthClientSecret' --named-value-id SAPOAuthClientSecret -g $resourceGroup --service-name $APIMserviceName --secret true --subscription $AzureSubscription --value $SAPOAuthClientSecret
-
 az apim nv create --display-name 'SAPOAuthScope' --named-value-id SAPOAuthScope -g $resourceGroup --service-name $APIMserviceName --secret false --subscription $AzureSubscription --value $SAPOAuthScope
-
 az apim nv create --display-name 'SAPOAuthServerAdressForTokenEndpoint' --named-value-id SAPOAuthServerAdressForTokenEndpoint -g $resourceGroup --service-name $APIMserviceName --secret false --subscription $AzureSubscription --value $SAPOAuthServerAdressForTokenEndpoint
-
 az apim nv create --display-name 'SAPOAuthRefreshExpiry' --named-value-id SAPOAuthRefreshExpiry -g $resourceGroup --service-name $APIMserviceName --secret false --subscription $AzureSubscription --value $SAPOAuthRefreshExpiry
